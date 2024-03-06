f744ca5e3b97b18090589dd82e907cde8489944c
Joey Fang <josudoey@gmail.com>
2024-03-06 10:49:49 +0800

    feat: add text to sha256 hex

--- packages/website/pages/home/index.js
+++ packages/website/pages/home/index.js
@@ -1,6 +1,7 @@
 import { reactive, toRefs } from 'vue'
 import Base64 from 'crypto-js/enc-base64'
 import Hex from 'crypto-js/enc-hex'
+import { SHA256 } from 'crypto-js'
 
 export function setup () {
   const data = reactive({
@@ -18,6 +19,14 @@ export function setup () {
           return
         }
         encoded.push(Base64.stringify(Hex.parse(line)))
+      }
+
+      data.result = encoded.join('\n')
+    },
+    textToSha256Hex () {
+      const encoded = []
+      for (const line of data.input.split('\n')) {
+        encoded.push(SHA256(line).toString(Hex))
       }
 
       data.result = encoded.join('\n')
